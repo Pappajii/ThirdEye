6879588275f7884f67f26c2ef77b73c0740019b1
Pappajii <49653653+Pappajii@users.noreply.github.com>
2021-09-05 21:14:29 +0530

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,21 @@
+const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+    const step = (timestamp) => {
+     if (!startTimestamp) startTimestamp = timestamp;
+     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+     target.innerText = Math.floor(progress * (end - start) + start);
+     if (progress < 1) {
+      window.requestAnimationFrame(step);
+     }
+    };
+    window.requestAnimationFrame(step);
+   };
+   //#endregion - end of - number counter animation
+   
+   document.addEventListener("DOMContentLoaded", () => {
+    counterAnim("#count1", 10, 50, 1000);
+    counterAnim("#count2", 50, 100, 1500);
+    counterAnim("#count3", 0, 2, 1000);
+    // counterAnim("#count4", 500, -100, 2500);
+   });
